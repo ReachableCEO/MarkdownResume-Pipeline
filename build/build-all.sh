b2ff9ecce9a65095ba2f54ff8d276282a560a91c
ReachableCEO <reachableceo@turnsys.com>
2024-12-09 13:13:30 -0600

    that took longer than i would have liked

--- build/build-all.sh
+++ build/build-all.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
 # shellcheck disable=SC1091
-source "$(dirname "${BASH_SOURCE[0]}")/bash3boilerplate.sh"
-
-# Load environment variables
-source "./ResumeVariables.env"
+#source "$(dirname "${BASH_SOURCE[0]}")/bash3boilerplate.sh"
 
 # Setup globals
+#readonly BUILD_OUTPUT_DIR="../build-output"
 readonly BUILD_TEMP_DIR="../build-temp"
-readonly BUILD_OUTPUT_DIR="../build-output"
-readonly BUILD_YAML_JOB_BOARD="$BUILD_TEMP_DIR/BuildJobBoard.yml"
-readonly BUILD_YAML_CLIENT_SUBMISSION="$BUILD_TEMP_DIR/BuildClientSubmission.yml"
+readonly BUILDYAML_JOBBOARD="$BUILD_TEMP_DIR/JobBoard.yml"
+readonly BUILDYAML_CLIENTSUBMISSION="$BUILD_TEMP_DIR/ClientSubmission.yml"
 
-readonly CANDIDATE_NAME="$CandidateName"
-readonly CANDIDATE_LOGO="$CandidateLogo"
-readonly CANDIDATE_TAGLINE="$CandidateTagline"
-readonly RESUME_SOURCE_CODE="$ResumeSourceCode"
-readonly URL_COLOR="$URLCOLOR"
-readonly PAGE_BACKGROUND="$PAGEBACKGROUND"
+#TODO make this work. For now, editing this script and setting the below export lines is required.
+#source ./ResumeVariables.env
 
-# Expand variables into rendered YAML files
-./mo "MarkdownResume-BuildTemplateInput-ClientSubmission.yml" > "$BUILD_YAML_CLIENT_SUBMISSION"
-./mo "MarkdownResume-BuildTemplateInput-JobBoard.yml" > "$BUILD_YAML_JOB_BOARD"
+export CandidateName="First Middle Last"
+export CandidateLogo=""
+export CandidateTagline="Your.Tagline.Here."
+export ResumeSourceCode="https://git.knownelement.com/reachableceo/MarkdownResume-Pipeline"
+export URLCOLOR="blue"
+export PAGEBACKGROUND="../vendor/git.knownelement.com/ExternalVendorCode/pandoc-latex-template/examples/page-background/backgrounds"
 
+# Expand variables into rendered YAML files. These will be used by pandoc to create the output artifacts
+
+./mo ./BuildTemplate-JobBoard.yml > $BUILDYAML_JOBBOARD
+./mo ./BuildTemplate-ClientSubmission.yml > $BUILDYAML_CLIENTSUBMISSION
